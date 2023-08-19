0cbe0ebe855350b80282c75e2a18a5bb84f9ec57
egarcode95 <egarcode95@gmail.com>
2023-08-18 20:36:47 -0600

    corrección de codigo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,22 @@
 
 function guessNumber(min, max) {
-
 	alert(`piensa en un número del ${min} al ${max}`);
-
 	let guess = Math.floor((max - min) / 2);
-
 	let currentMin = min;
 	let currentMax = max;
 	let keepGuessing = true;
-	const results = [];	
-	results.push({guess, currentMin, currentMax});	
-	while (keepGuessing) {
+		while (keepGuessing) {
 		if (confirm(`¿Tu número es mayor que ${guess}?`)) {
 			currentMin = guess + 1;
 			guess = Math.floor((currentMax - currentMin) / 2 + currentMin);
 		} else if (confirm(`¿Tu número es ${guess}?`)) {
 			keepGuessing = false;
-			break;  
-		} else {
+			} else {
 			currentMax = guess - 1;
 			guess = Math.floor((currentMax - currentMin) / 2 + currentMin);
 		}
 		if (currentMin === currentMax) keepGuessing = false;
-		results.push({guess, currentMin, currentMax});
 	}
-	console.table(results);	
 	alert(`Tu número es ${guess}, excelente decisión.`)
 	return guess;
 }
